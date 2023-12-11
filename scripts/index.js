2c74d14937da5ed9cdbaae59bd9257c8ed56071f
Elisemurph01 <elisem01@hotmail.com>
2023-12-10 22:20:54 -0800

    js refactor

--- scripts/index.js
+++ scripts/index.js
@@ -30,16 +30,36 @@ const initialCards = [
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const modalCloseButton = profileEditModal.querySelector("#modal-close-button");
+const profileName = document.querySelector('.profile__title');
+const profileDescription = document.querySelector('.profile__description');
+const profileTitleInput = document.querySelector('#profile-title-input');
+const profileDescriptionInput = document.querySelector('#profile-description-input');
+const profileEditForm = profileEditModal.querySelector('.modal__form');
 
 /* FUNCTIONS */
 
 function closePopup() {
     profileEditModal.classList.remove("modal_opened");
-  }
+}
+  
+
+/* EVENT HANDLERS */
+
+function handleProfileEditSubmit(e) {
+    e.preventDefault();
+    profileName.textContent = profileTitleInput.value;
+    profileDescription.textContent = profileDescriptionInput.value;
+    closePopup();
+}
 
 /* EVENT LISTENERS */
 
 profileEditButton.addEventListener('click', () => {
+    profileTitleInput.value = profileName.textContent;
+    profileDescriptionInput.value = profileDescription.textContent;
     profileEditModal.classList.add("modal_opened")
 });
 modalCloseButton.addEventListener('click', closePopup);
+
+profileEditForm.addEventListener('submit', handleProfileEditSubmit);
+
