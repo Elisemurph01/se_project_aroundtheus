a5cb93bcb850519fbe6ad3961bc2da569269eb16
Elisemurph01 <elisem01@hotmail.com>
2024-01-02 16:19:58 -0800

    update html

--- scripts/validation.js
+++ scripts/validation.js
@@ -66,7 +66,7 @@ const config = {
     formSelector: ".popup__form",
     inputSelector: ".popup__input",
     submitButtonSelector: ".popup__button",
-    inactiveButtonClass: "popup__button_disabled",
+    inactiveButtonClass: "modal__button_disabled",
     inputErrorClass: "popup__input_type_error",
     errorClass: "popup__error_visible"
 };
