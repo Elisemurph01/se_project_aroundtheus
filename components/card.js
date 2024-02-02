20e78b2654fff03afeb735bfc68237fe91ff2103
Elisemurph01 <elisem01@hotmail.com>
2024-02-02 13:14:07 -0800

    update to cards

--- components/card.js
+++ components/card.js
@@ -44,10 +44,10 @@ export default class Card {
         this._likeButton = this._cardElement.querySelector(".cards__like-button");
         this._trashButton = this._cardElement.querySelector(".cards__trash-button");
         this._cardImageEl = this._cardElement.querySelector(".cards__image");
-        this._cardTitleEl = this._cardElement.querySelector(".cards__description");
+        this._cardTitleEl = this._cardElement.querySelector(".cards__title");
         this._cardTemplate = this._cardElement.querySelector("#card-template");
     
-        this._cardTitleEl.textContent = this.name;
+        this._cardTitleEl.textContent = this._name;
         this._cardImageEl.setAttribute("src", this._link);
         this._cardImageEl.setAttribute("alt", this._name);
 
